2d6f9c1324e627a9a391c02ef315046644d7b728
Billgo <cocobill@vip.qq.com>
2019-06-23 15:58:57 +0800

    update new version

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
-const connect = require('gulp-connect');
-const rename = require('gulp-rename');
-const rimraf = require('rimraf');
-const ejs = require('gulp-ejs');
-const LessPluginAutoPrefix = require('less-plugin-autoprefix');
-const LessPluginInlineUrls = require('less-plugin-inline-urls');
-const LessPluginFunctions = require('less-plugin-functions');
-const fs = require('fs');
+const gulp = require("gulp");
+const less = require("gulp-less");
+const connect = require("gulp-connect");
+const rename = require("gulp-rename");
+const rimraf = require("rimraf");
+const ejs = require("gulp-ejs");
+const LessPluginAutoPrefix = require("less-plugin-autoprefix");
+const LessPluginInlineUrls = require("less-plugin-inline-urls");
+const LessPluginFunctions = require("less-plugin-functions");
+const fs = require("fs");
 
 const autoprefixPlugin = new LessPluginAutoPrefix({
-  browsers: ['> 5%', 'ie >= 8'],
+  browsers: ["> 5%", "ie >= 8"],
 });
 
 const lessDevConfig = {
@@ -22,28 +22,28 @@ const getVariablesFromLess = (files = []) => {
   const lessVariables = {};
   files.forEach(file => {
     const filePath = `./src/variables/${file}.less`;
-    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const fileContent = fs.readFileSync(filePath, "utf8");
     const matches = fileContent.match(/@(.+):/g);
     matches.forEach(item => {
-      lessVariables[item.replace(/[:@]/g, '')] = true;
+      lessVariables[item.replace(/[:@]/g, "")] = true;
     });
   });
   return lessVariables;
 };
 
-gulp.task('default', function() {
-  const vars = getVariablesFromLess(['color', 'fonts', 'sizes']);
+gulp.task("default", function() {
+  const vars = getVariablesFromLess(["color", "fonts", "sizes"]);
   return gulp
-    .src('./scripts/extract-vars.less')
+    .src("./scripts/extract-vars.less")
     .pipe(
       ejs({
         vars: Object.keys(vars),
       }),
     )
     .pipe(less(lessDevConfig))
-    .pipe(gulp.dest('./src'))
-    .on('end', function() {
-      const cssFile = fs.readFileSync(`./src/extract-vars.css`, 'utf8');
+    .pipe(gulp.dest("./src"))
+    .on("end", function() {
+      const cssFile = fs.readFileSync(`./src/extract-vars.css`, "utf8");
       const matches = cssFile.match(/\.(.+) {\n\s+color: (.+)\n}/g);
       const varsArr = [];
       matches.forEach(match => {
@@ -52,15 +52,15 @@ gulp.task('default', function() {
         });
       });
       gulp
-        .src('./scripts/extract-vars.js')
+        .src("./scripts/extract-vars.js")
         .pipe(
           ejs({
             vars: varsArr,
           }),
         )
         .pipe(rename(`index.js`))
-        .pipe(gulp.dest('./src'))
-        .on('end', () => {
+        .pipe(gulp.dest("./src"))
+        .on("end", () => {
           rimraf(`./src/extract-vars.css`, () => {});
         });
     });
--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   verbose: true,
   collectCoverage: true,
-  coverageReporters: ['html', 'text', 'json-summary'],
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  coveragePathIgnorePatterns: ['/node_modules/', '(.stories)\\.(js)$'],
-  moduleDirectories: ['node_modules', 'src'],
+  coverageReporters: ["html", "text", "json-summary"],
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coveragePathIgnorePatterns: ["/node_modules/", "(.stories)\\.(js)$"],
+  moduleDirectories: ["node_modules", "src"],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/mocks/fileMock.js',
-    '\\.(css|less|scss)$': '<rootDir>/mocks/styleMock.js',
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/mocks/fileMock.js",
+    "\\.(css|less|scss)$": "<rootDir>/mocks/styleMock.js",
   },
-  testEnvironment: 'node',
+  testEnvironment: "node",
   testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/build/',
-    '<rootDir>/dist/',
-    '<rootDir>/lib/',
-    '<rootDir>/umd/',
+    "<rootDir>/node_modules/",
+    "<rootDir>/build/",
+    "<rootDir>/dist/",
+    "<rootDir>/lib/",
+    "<rootDir>/umd/",
   ],
-  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.jsx?$',
+  testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.jsx?$",
   transform: {
-    '^.+\\.js$': 'babel-jest',
+    "^.+\\.js$": "babel-jest",
   },
 };
